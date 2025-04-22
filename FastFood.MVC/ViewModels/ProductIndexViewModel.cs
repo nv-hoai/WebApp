54cfb62e6357589f142583580706d4b598522db1
nv-hoai <nguyenhoai0990@gmail.com>
2025-04-22 09:20:36 +0700

    Layout, page management

---
+++ FastFood.MVC/ViewModels/ProductIndexViewModel.cs
@@ -0,0 +1,10 @@
+﻿using FastFood.MVC.Models;
+
+namespace FastFood.MVC.ViewModels
+{
+    public record ProductIndexViewModel
+    (
+        IList<Category> Categories,
+        IEnumerable<Product> Products
+    );
+}
