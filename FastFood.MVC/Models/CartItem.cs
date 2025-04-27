6ec9cda79c0b8194e2750d4cd0cab0fc474b6cb7
YDraGN <nguyenthibaongan.ydragn@gmail.com>
2025-04-27 21:32:53 +0700

    Create CartItem.cs

---
+++ FastFood.MVC/Models/CartItem.cs
@@ -0,0 +1,19 @@
+﻿namespace FastFood.MVC.Models
+{
+    public class CartItem
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; } = "";
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public decimal SubTotal => Price * Quantity;
+        public int? PromotionID { get; set; }
+        public decimal DiscountPercent { get; set; }
+        public decimal CalculateSubTotal ()
+        {
+            return (PromotionID.HasValue ? 
+                    Price * Quantity * (1 - DiscountPercent) : 
+                    Price * Quantity);
+        }
+    }
+}
