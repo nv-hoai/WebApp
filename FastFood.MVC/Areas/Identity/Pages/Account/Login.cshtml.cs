d90c00dd9ebf046583caf65aab62e863c8fc7af5
bnguien <nguyennguyendangbao8@gmail.com>
2025-05-04 11:56:33 +0700

    Update Login.cshtml.cs

--- FastFood.MVC/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ FastFood.MVC/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -22,11 +22,13 @@ namespace FastFood.MVC.Areas.Identity.Pages.Account
     {
         private readonly SignInManager<ApplicationUser> _signInManager;
         private readonly ILogger<LoginModel> _logger;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public LoginModel(SignInManager<ApplicationUser> signInManager, ILogger<LoginModel> logger)
+        public LoginModel(SignInManager<ApplicationUser> signInManager, ILogger<LoginModel> logger, UserManager<ApplicationUser> userManager)
         {
             _signInManager = signInManager;
             _logger = logger;
+            _userManager = userManager;
         }
 
         /// <summary>
@@ -116,6 +118,12 @@ namespace FastFood.MVC.Areas.Identity.Pages.Account
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User logged in.");
+                    var user = await _userManager.FindByEmailAsync(Input.Email);
+                    var roles = await _userManager.GetRolesAsync(user);
+                    if (roles.Contains("Admin"))
+                    {
+                        return RedirectToAction("DashBoard", "Admin", new { area = "" }); 
+                    }
                     return LocalRedirect(returnUrl);
                 }
                 if (result.RequiresTwoFactor)
